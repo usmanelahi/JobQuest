40cc942a68f34e4c156dc484b87e72389c99fc42
usmanelahi <usmanelahi0201@gmail.com>
2023-06-30 18:25:52 +0500

    Dependencies Fixed

--- app/index.js
+++ app/index.js
@@ -1,7 +1,11 @@
 import { View, Text } from "react-native";
 
 const Home = () => {
-  return <View>Home</View>;
+  return (
+    <View>
+      <Text> Home </Text>
+    </View>
+  );
 };
 
 export default Home;
